de352932fe5c643aa2a866feba47437e5bc8dd5f
Onwunali Tochukwu <onwunali91@yahoo.com>
2022-09-12 23:12:54 +0100

    done prod v2.7

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,10 +41,10 @@ export default function About(props) {
                           data-aos-delay="1000" 
                         >As an online food vendor, your two greatest assets are money and time and we help you save both. On ryusen, you can buy all the groceries and supplies you need to cook at highly affordable prices and deliver meals to your customers easily and quickly. This way you can focus on just making tasty meals and profit. It’s a full course meal on the ryusen side of life. Who wouldn’t want in?</p>
                         <button 
-                          data-aos="fade-in" 
-                          data-aos-duration="2000"
-                          data-aos-easing="ease-in-out"
-                          data-aos-delay="1500"
+                          // data-aos="fade-in" 
+                          // data-aos-duration="2000"
+                          // data-aos-easing="ease-in-out"
+                          // data-aos-delay="1500"
                           className='btn'
                         >Count Me In!</button>
                     </div>
